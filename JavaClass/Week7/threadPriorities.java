1e4f1a84c29028ed8e43a834e8425ff5fd98e7d4
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-07 18:31:19 +0530

    java

--- JavaClass/Week7/threadPriorities.java
+++ JavaClass/Week7/threadPriorities.java
@@ -4,7 +4,7 @@ class test extends Thread
 {
      public void run()
      {
-          System.out.println(Thread.currentThread().getPriority());
+          System.out.println(Thread.currentThread().getPriority()); //getPriority is a built in function
      }
 }
 
@@ -15,9 +15,12 @@ class threadPriorities {
           test thrd2=new test();
           test thrd3=new test();
 
-          thrd1.setPriority(Thread.MIN_PRIORITY);
+          thrd1.setPriority(Thread.MIN_PRIORITY); //Thread.MIN_PRIORITY -> Parameter
+          //Syntax of MINIMUM PRIORITY - public static final int MIN_PRIORITY=1
           thrd2.setPriority(Thread.NORM_PRIORITY);
+          //Syntax of NORMAL PRIORITY - public static final int NORM_PRIORITY=5
           thrd3.setPriority(Thread.MAX_PRIORITY);
+          //Syntax of MAXIMUM PRIORITY - public static final int MAX_PRIORITY=10
 
           thrd1.start();
           thrd2.start();
