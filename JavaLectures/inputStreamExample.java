39f4e100ee6e09ea905b952f1f40df3c1c769527
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-21 12:49:01 +0530

    java

--- JavaLectures/inputStreamExample.java
+++ JavaLectures/inputStreamExample.java
@@ -1,26 +1,26 @@
-import java.io.*;
-import java.io.FileInputStream;
+// import java.io.*;
+// import java.io.FileInputStream;
 
-class inputStreamExample
-{
-     public static void main(String[] args)
-     {
-          byte b[]=new byte[100];
+// class inputStreamExample
+// {
+//      public static void main(String[] args)
+//      {
+//           byte b[]=new byte[100];
 
-          InputStream in=new FileInputStream("E:\\NewFolder\\data2.txt");
+//           InputStream in=new FileInputStream("E:\\NewFolder\\data2.txt");
 
-          try{
-               in.read(b);
-               System.out.println("Bytes Available in the File: "+in.available());
-               String str=new String();
-               System.out.println(str);
+//           try{
+//                in.read(b);
+//                System.out.println("Bytes Available in the File: "+in.available());
+//                String str=new String();
+//                System.out.println(str);
 
-               in.close();
-          }
+//                in.close();
+//           }
 
-          catch (Exception e)
-          {
-               System.out.println(e);
-          }
-     }
-}
+//           catch (Exception e)
+//           {
+//                System.out.println(e);
+//           }
+//      }
+// }
