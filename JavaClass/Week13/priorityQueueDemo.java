e25efb93e00272862bc80c276df49cea6dd1b1d6
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-04-25 13:26:29 +0530

    java

--- JavaClass/Week13/priorityQueueDemo.java
+++ JavaClass/Week13/priorityQueueDemo.java
@@ -2,6 +2,7 @@
 package JavaClass.Week13;
 
 import java.util.PriorityQueue;
+// import java.rmi.Remote;
 
 class priorityQueueDemo {
      public static void main(String[] args)
@@ -12,6 +13,17 @@ class priorityQueueDemo {
           System.out.println("P.Q.: "+numbers);
           numbers.offer(1);
           System.out.println("P.Q.: "+numbers);
-          //l
+          System.out.println();
+
+          System.out.println(numbers.peek()); //Returns the head of the PQ, ie, here, 1
+          System.out.println(numbers.remove(1)); //Removes the specified position element and prints true if the element is removed
+          System.out.println(numbers); //Prints the present elements, ie, here, [1, 2]
+          System.out.println(numbers.poll()); //Removes the head of the Queue, ie, here, 1
+          System.out.println(numbers);
+          System.out.println();
+
+          boolean k=numbers.contains(4);
+          System.out.println(k);
+          System.out.println(numbers.size());
      }
 }
