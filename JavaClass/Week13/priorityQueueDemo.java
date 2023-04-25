75372ec09e6ada277e9e858a7b7a473ff094e286
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-04-25 13:17:16 +0530

    java

---
+++ JavaClass/Week13/priorityQueueDemo.java
@@ -0,0 +1,16 @@
+//PriorityQueue Class
+package JavaClass.Week13;
+
+import java.util.PriorityQueue;
+
+class priorityQueueDemo {
+     public static void main(String[] args)
+     {
+          PriorityQueue<Integer> numbers=new PriorityQueue<Integer>();
+          numbers.add(4);
+          numbers.add(2);
+          System.out.println("P.Q.: "+numbers);
+          numbers.offer(1);
+          System.out.println("P.Q.: "+numbers);
+     }
+}
